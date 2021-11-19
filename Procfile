ee477d50f2b0835beec4c40e9681d05fc802573b
Drew Nollsch <drew.nollsch@gmail.com>
2021-11-19 17:56:38 +0100

    added express webserver

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn myapp.wsgi
+web: npm start
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+var port = process.env.PORT || 80
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
