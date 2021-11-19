c2657baef36640a963ec36d15edaf67a7d54a9b2
Drew Nollsch <drew.nollsch@gmail.com>
2021-11-19 17:44:24 +0100

    removed typo in App.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import axios from "axios";
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { l, 
+        this.state = { 
             searchValue: '',
             searchResults: [],
             loading: true
